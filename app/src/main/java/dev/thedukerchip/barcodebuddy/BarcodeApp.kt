b13ceadc354e134708f4f1980bc6b5d2eb17e5c8
Karthik Sankar <thedukerchip@gmail.com>
2021-06-13 09:34:43 +0530

    Hilt DI configured

---
+++ app/src/main/java/dev/thedukerchip/barcodebuddy/BarcodeApp.kt
@@ -0,0 +1,7 @@
+package dev.thedukerchip.barcodebuddy
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class BarcodeApp: Application()
--- app/src/main/java/dev/thedukerchip/barcodebuddy/ui/launcher/LauncherActivity.kt
+++ app/src/main/java/dev/thedukerchip/barcodebuddy/ui/launcher/LauncherActivity.kt
@@ -1,9 +1,12 @@
-package dev.thedukerchip.barcodebuddy
+package dev.thedukerchip.barcodebuddy.ui.launcher
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import dagger.hilt.android.AndroidEntryPoint
+import dev.thedukerchip.barcodebuddy.R
 
-class MainActivity : AppCompatActivity() {
+@AndroidEntryPoint
+class LauncherActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
