ad4e35419b3c110a578e34bb701dab46906892c5
Karthik Sankar <thedukerchip@gmail.com>
2021-06-13 10:05:32 +0530

    Creating separate res folders for each ui module 	Databinding configured

--- app/src/main/java/dev/thedukerchip/barcodebuddy/ui/launcher/LauncherActivity.kt
+++ app/src/main/java/dev/thedukerchip/barcodebuddy/ui/launcher/LauncherActivity.kt
@@ -4,11 +4,17 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import dagger.hilt.android.AndroidEntryPoint
 import dev.thedukerchip.barcodebuddy.R
+import dev.thedukerchip.barcodebuddy.databinding.ActivityLauncherBinding
 
 @AndroidEntryPoint
 class LauncherActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLauncherBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityLauncherBinding.inflate(layoutInflater)
+
+        setContentView(R.layout.activity_launcher)
     }
 }
