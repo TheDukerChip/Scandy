ecb7ac529d786a7e9fc0884360e03c7d9304fcac
Karthik Sankar <thedukerchip@gmail.com>
2021-07-03 17:04:49 +0530

    Launcher screen design completed

--- app/src/main/java/dev/thedukerchip/scandy/ui/launcher/LauncherActivity.kt
+++ app/src/main/java/dev/thedukerchip/scandy/ui/launcher/LauncherActivity.kt
@@ -1,13 +1,26 @@
 package dev.thedukerchip.scandy.ui.launcher
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
+import dev.thedukerchip.scandy.ui.scanner.ScannerActivity
+import kotlinx.coroutines.delay
+import scandy.R
 
 class LauncherActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        TODO("Render the launcher UI")
-        TODO("Start scanner activity")
+        setContentView(R.layout.activity_launcher)
+
+        lifecycleScope.launchWhenCreated {
+            delay(2_000)
+            openScanner()
+        }
+    }
+
+    private fun openScanner() {
+        startActivity(Intent(this, ScannerActivity::class.java))
     }
 }
